b91515642e3836a098cec0801651fec3ac64dc44
Humberto Posadas <posadas.humberto@outlook.com>
2023-04-28 17:18:46 +0200

    adding list client with RestTemplate handling many types of ResponseEntity

--- src/main/java/org/hposadas/springresttemplate/client/BeerClient.java
+++ src/main/java/org/hposadas/springresttemplate/client/BeerClient.java
@@ -1,2 +1,9 @@
-package org.hposadas.springresttemplate.client;public interface BeerClient {
+package org.hposadas.springresttemplate.client;
+
+import org.hposadas.springresttemplate.model.BeerDTO;
+import org.springframework.data.domain.Page;
+
+public interface BeerClient {
+
+    Page<BeerDTO> listBeers();
 }
