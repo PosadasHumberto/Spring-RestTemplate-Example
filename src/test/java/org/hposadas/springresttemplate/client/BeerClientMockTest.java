cab0315552870f4cfff4fe850884aa50e966903f
Humberto Posadas <posadas.humberto@outlook.com>
2023-04-29 20:36:11 +0200

    Implementing tests method Creating Beer

--- src/test/java/org/hposadas/springresttemplate/client/BeerClientMockTest.java
+++ src/test/java/org/hposadas/springresttemplate/client/BeerClientMockTest.java
@@ -19,14 +19,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withAccepted;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RestClientTest
@@ -81,6 +84,29 @@ public class BeerClientMockTest {
                 .andRespond(withSuccess(response, MediaType.APPLICATION_JSON));
 
         BeerDTO responseDTO = beerClient.getBeerById(beerDTO.getId());
+
+        assertThat(responseDTO.getId()).isEqualTo(beerDTO.getId());
+    }
+
+    @Test
+    void testCreateBeer() throws JsonProcessingException {
+
+        BeerDTO beerDTO = getBeerDto();
+        String response = objectMapper.writeValueAsString(beerDTO);
+        URI uri = UriComponentsBuilder.fromPath(BeerClientImpl.GET_BEER_BY_ID_PATH)
+                .build(beerDTO.getId());
+
+        server.expect(method(HttpMethod.POST))
+                .andExpect(requestTo(
+                        URL + BeerClientImpl.GET_BEER_PATH))
+                .andRespond(withAccepted().location(uri));
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestToUriTemplate(
+                        URL+BeerClientImpl.GET_BEER_BY_ID_PATH, beerDTO.getId()))
+                .andRespond(withSuccess(response, MediaType.APPLICATION_JSON));
+
+        BeerDTO responseDTO = beerClient.createBeer(beerDTO);
 
         assertThat(responseDTO.getId()).isEqualTo(beerDTO.getId());
     }
